bcb4d08daf57bc8a6ace23cd6e637da6808cc50a
Stefan Wick <wick.stefan@outlook.com>
2018-04-22 06:39:58 -0700

    auto-launch with audio sample

---
+++ AutoLaunchWithAudio/AutoLaunchWithAudio/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.System;
+using Windows.Media.Playback;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.ExtendedExecution.Foreground;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AutoLaunchWithAudio
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ExtendedExecutionForegroundSession session = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void MediaPlayer_MediaFailed(MediaPlayer sender, MediaPlayerFailedEventArgs args)
+        {
+            MessageDialog dlg = new MessageDialog(args.ErrorMessage, "MediaFailed");
+            await dlg.ShowAsync();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            player.Source = MediaSource.CreateFromUri(new Uri("http://live-aacplus-64.kexp.org/kexp64.aac"));
+            player.MediaPlayer.MediaFailed += MediaPlayer_MediaFailed;
+            player.MediaPlayer.CurrentStateChanged += MediaPlayer_CurrentStateChanged;
+        }
+
+        private async void MediaPlayer_CurrentStateChanged(MediaPlayer sender, object args)
+        {
+            if (sender.PlaybackSession.PlaybackState == MediaPlaybackState.Playing)
+            {
+                session = new ExtendedExecutionForegroundSession();
+                session.Reason = ExtendedExecutionForegroundReason.BackgroundAudio;
+                var result = await session.RequestExtensionAsync();
+                if (result != ExtendedExecutionForegroundResult.Allowed)
+                    throw new Exception("EE denied");
+
+                IList<AppDiagnosticInfo> infos = await AppDiagnosticInfo.RequestInfoForAppAsync();
+                IList<AppResourceGroupInfo> resourceInfos = infos[0].GetResourceGroups();
+                await resourceInfos[0].StartSuspendAsync();
+            }
+        }
+    }
+}
---
+++ AutoLaunchWithAudio/DesktopExtension/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+
+namespace DesktopExtension
+{
+    class Program
+    {
+        static private AutoResetEvent resetEvent;
+        static void Main(string[] args)
+        {
+            resetEvent = new AutoResetEvent(false);
+            InvokeForegroundApp();
+            resetEvent.WaitOne();
+        }
+
+        static private async void InvokeForegroundApp()
+        {
+            var appListEntries = await Package.Current.GetAppListEntriesAsync();
+            await appListEntries.First().LaunchAsync();
+            resetEvent.Set();
+        }
+    }
+}
