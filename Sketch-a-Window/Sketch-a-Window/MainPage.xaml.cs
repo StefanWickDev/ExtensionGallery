d58d9b5d322aaf6f2e8066aeeb2236b2f729a2f3
Stefan Wick <wick.stefan@hotmail.com>
2018-12-18 10:36:54 -0800

    adding a new sample Sketch-a-Window

---
+++ Sketch-a-Window/Sketch-a-Window/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.UI.Input.Inking;
+using Windows.Foundation.Metadata;
+using Windows.ApplicationModel;
+using Windows.UI.ViewManagement;
+using Windows.Storage;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+
+namespace Sketch_a_Window
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView().TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            ApplicationView.GetForCurrentView().TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            ApplicationView.GetForCurrentView().TitleBar.ButtonForegroundColor = Colors.Black;
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+
+            inkCanvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Touch;
+            inkCanvas.InkPresenter.StrokesCollected += InkPresenter_StrokesCollected;
+        }
+
+        private async void InkPresenter_StrokesCollected(InkPresenter sender, InkStrokesCollectedEventArgs args)
+        {
+            Rect viewBounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            Rect inkBounds = args.Strokes[0].BoundingRect;
+            ApplicationData.Current.LocalSettings.Values["X"] = viewBounds.X + inkBounds.Left;
+            ApplicationData.Current.LocalSettings.Values["Y"] = viewBounds.Y + inkBounds.Top;
+            ApplicationData.Current.LocalSettings.Values["Width"] = Window.Current.Bounds.Width;
+            ApplicationData.Current.LocalSettings.Values["Height"] = Window.Current.Bounds.Height;
+            
+            var inkPoints = args.Strokes[0].GetInkPoints();
+            var rawPoints = new double[inkPoints.Count * 2];
+            for(int i=0; i<inkPoints.Count; i++)
+            {
+                rawPoints[2 * i]     = inkPoints[i].Position.X - inkBounds.Left;
+                rawPoints[2 * i + 1] = inkPoints[i].Position.Y - inkBounds.Top;
+            }
+            SavePointsToSettings(rawPoints);
+            
+
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            }
+            await Task.Delay(1000);
+            sender.StrokeContainer.Clear();
+        }
+
+        private void SavePointsToSettings(double []points)
+        {
+            try
+            {
+                ApplicationData.Current.LocalSettings.Values["Points"] = points;
+            }
+            catch
+            {
+                double []reducedPoints = new double[points.Length / 2];
+                for (int i=0; i<reducedPoints.Length-1; i+=2)
+                {
+                    reducedPoints[i] = points[2 * i];
+                    reducedPoints[i+1] = points[2 * i +1];
+                }
+                SavePointsToSettings(reducedPoints);
+            }
+        }
+    }
+}
---
+++ Sketch-a-Window/Sketch-a-Window/WPF/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Storage;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random(Environment.TickCount);
+        public MainWindow()
+        {
+            this.Left = (double)ApplicationData.Current.LocalSettings.Values["X"];
+            this.Top = (double)ApplicationData.Current.LocalSettings.Values["Y"];
+            this.Width = (double)ApplicationData.Current.LocalSettings.Values["Width"];
+            this.Height = (double)ApplicationData.Current.LocalSettings.Values["Height"];
+
+            InitializeComponent();
+            this.MouseMove += MainWindow_MouseMove;
+
+            RadialGradientBrush brush = new RadialGradientBrush();
+            brush.GradientStops.Add(new GradientStop(GetRandomColor(), 0.0d));
+            brush.GradientStops.Add(new GradientStop(GetRandomColor(), 0.5d));
+            brush.GradientStops.Add(new GradientStop(GetRandomColor(), 1.0d));
+            root.Background = brush;
+
+            double[] rawPoints = (double[])ApplicationData.Current.LocalSettings.Values["Points"];
+            PathGeometry geo = new PathGeometry();
+            PathFigure figure = new PathFigure();
+            figure.StartPoint = new Point(rawPoints[0], rawPoints[1]);
+            PolyLineSegment segment = new PolyLineSegment();
+            for (int i = 0; i < rawPoints.Length / 2; i+=2)
+            {
+                segment.Points.Add(new Point(rawPoints[2 * i], rawPoints[2 * i +1]));
+            }
+            figure.Segments.Add(segment);
+            geo.Figures.Add(figure);
+            this.Clip = geo;
+
+            closeButton.SetValue(Canvas.LeftProperty, geo.Bounds.Width / 2 - geo.Bounds.Left - 16);
+            closeButton.SetValue(Canvas.TopProperty, geo.Bounds.Height / 2 - geo.Bounds.Top - 16);
+        }
+
+        private Color GetRandomColor()
+        {
+            byte[] bytes = new byte[4];
+            rnd.NextBytes(bytes);
+            return Color.FromArgb((byte)(192 + bytes[0]/4), bytes[1], bytes[2], bytes[3]);
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                {
+                    this.DragMove();
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
